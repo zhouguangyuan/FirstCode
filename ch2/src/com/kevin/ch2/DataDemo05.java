48ecbb6ce2419f3f5ff1757aa22df6cc85b31708
Kevin Zhou <kevinsoft@126.com>
2019-04-22 17:09:07 +0800

    Learn the annotation
    The single and mutil line annotation.

---
+++ ch2/src/com/kevin/ch2/DataDemo05.java
@@ -0,0 +1,14 @@
+package com.kevin.ch2;
+
+public class DataDemo05 {
+
+	public static void main(String[] args) {
+		char ch1 = '\"';
+		char ch2 = '\\';
+		System.out.println("ch1="+ch1);
+		System.out.println("ch2="+ch2);
+		System.out.println("\"Hello World\"");
+
+	}
+
+}
